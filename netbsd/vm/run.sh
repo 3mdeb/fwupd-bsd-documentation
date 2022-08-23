3eed8037682f26f6e66da8b124bcd8c9c47cace3
Paweł Cichowski <pawel.cichowski@3mdeb.com>
2022-08-23 12:45:12 +0200

    netbsd: setup.sh: script now deletes previous disk image

--- netbsd/vm/run.sh
+++ netbsd/vm/run.sh
@@ -13,6 +13,7 @@ fi
 qemu-system-x86_64 -m 2048 \
                    -drive if=virtio,file=disk.qcow2,format=qcow2 \
                    -enable-kvm \
+                   -display gtk \
                    -netdev user,id=mynet0,hostfwd=tcp:127.0.0.1:9272-:22 \
                    -device virtio-net,netdev=mynet0 \
                    -bios OVMF.fd \
