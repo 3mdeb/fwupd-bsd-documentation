91dc220f6466602cfd3f30b4de25d7de11edcb7f
Michał Kotyla <michal.kotyla@3mdeb.com>
2022-08-24 14:17:40 +0200

    dragonflybsd: initial version of documentation

---
+++ dragonflybsd/vm/run.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f disk.qcow2 ]; then
+    echo "disk.qcow2 doesn't exists! Run setup.sh first."
+    exit 1
+fi
+
+echo "Running QEMU..."
+qemu-system-x86_64 -m 2048 \
+                   -drive if=virtio,file=disk.qcow2,format=qcow2 \
+                   -enable-kvm \
+                   -display gtk \
+                   -netdev user,id=mynet0,hostfwd=tcp:127.0.0.1:9272-:22 \
+                   -device virtio-net,netdev=mynet0 \
+                   -bios OVMF.fd \
+                   -smp 6 \
+                   -s \
+                   -cpu host
